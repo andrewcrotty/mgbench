0f78a309fc20412e13ed3bb10906db84bc9b5c6e
Andrew Crotty <crottyan@gmail.com>
2020-04-28 16:58:36 -0400

    monetdb load

--- tools/monetdb.sql
+++ tools/monetdb.sql
@@ -1,4 +1,6 @@
 --mclient -u monetdb -d mgbench -t performance
 --password: monetdb
-COPY 22023859 OFFSET 2 RECORDS INTO logs FROM '/data/agg/ddd/bench1.csv' USING DELIMITERS ',' NULL AS '';
-COPY 75748118 OFFSET 2 RECORDS INTO logs FROM '/data/agg/ddd/bench2.csv' USING DELIMITERS ',' NULL AS '';
+
+--load
+COPY 22023859 OFFSET 2 RECORDS INTO logs FROM '/path/to/bench1.csv' USING DELIMITERS ',' NULL AS '';
+COPY 75748118 OFFSET 2 RECORDS INTO logs FROM '/path/to/bench2.csv' USING DELIMITERS ',' NULL AS '';
