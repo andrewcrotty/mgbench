04627eb9e15b2756501ec0b74b905f7f75f965f5
Andrew Crotty <crottyan@gmail.com>
2020-05-01 10:28:55 -0400

    update cratedb

--- install/cratedb.sh
+++ install/cratedb.sh
@@ -2,3 +2,6 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 bash -c "$(curl -L https://try.crate.io/)"
+curl -o crash https://cdn.crate.io/downloads/releases/crash_standalone_latest
+chmod +x crash
+#./crash
