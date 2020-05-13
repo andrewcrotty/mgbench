9e3d7934095e397530cf818cd5334b70d8c9f4a1
Andrew Crotty <crottyan@gmail.com>
2020-05-13 13:43:30 -0400

    bench2 ddl cleanup

--- bench2/ddl_monetdb.sql
+++ bench2/ddl_monetdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE logs (
   log_time    TIMESTAMP NOT NULL,
-  client_ip   VARCHAR(16) NOT NULL,
+  client_ip   INET NOT NULL,
   request     VARCHAR(1000) NOT NULL,
   status_code SMALLINT NOT NULL,
   object_size BIGINT NOT NULL
