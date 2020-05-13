21fd352d02f8e42caa3d6f133a93352e8cb01d32
Andrew Crotty <crottyan@gmail.com>
2020-05-13 16:02:50 -0400

    druid install

---
+++ install/druid.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+wget --trust-server-names "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=druid/0.18.0/apache-druid-0.18.0-bin.tar.gz"
+tar -xzf apache-druid-0.18.0-bin.tar.gz
+cd apache-druid-0.18.0
+./bin/start-single-server-xlarge
