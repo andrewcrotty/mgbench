d888036c1f17c2c7f79fcfe372f424e0676c8790
Andrew Crotty <crottyan@gmail.com>
2020-05-19 10:55:15 -0400

    clickhouse fixes

---
+++ bench1/q3_clickhouse.sql
@@ -0,0 +1,28 @@
+SELECT dt,
+       hr,
+       AVG(load_fifteen) AS avg_load_fifteen,
+       AVG(load_five) AS avg_load_five,
+       AVG(load_one) AS avg_load_one,
+       AVG(mem_free) AS avg_mem_free,
+       AVG(swap_free) AS avg_swap_free
+FROM (
+  SELECT CAST(log_time AS DATE) AS dt,
+         EXTRACT(HOUR FROM log_time) AS hr,
+         load_fifteen,
+         load_five,
+         load_one,
+         mem_free,
+         swap_free
+  FROM logs
+  WHERE machine_name = 'babbage'
+    AND load_fifteen IS NOT NULL
+    AND load_five IS NOT NULL
+    AND load_one IS NOT NULL
+    AND mem_free IS NOT NULL
+    AND swap_free IS NOT NULL
+    AND log_time >= TIMESTAMP '2017-01-01 00:00:00'
+) AS r
+GROUP BY dt,
+         hr
+ORDER BY dt,
+         hr;
