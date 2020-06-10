cbe66408aab4e2a681f060845feb4ad1cc266504
Andrew Crotty <crottyan@gmail.com>
2020-06-10 12:42:57 -0400

    q2.6 fixes

--- bench2/q6/q6.sql
+++ bench2/q6/q6.sql
@@ -1,7 +1,7 @@
 -- Q2.6: What are the average and maximum data transfer rates (Gbps)?
 
-SELECT AVG(r.transfer) / 125000000.0 AS transfer_avg,
-       MAX(r.transfer) / 125000000.0 AS transfer_max
+SELECT AVG(transfer) / 125000000.0 AS transfer_avg,
+       MAX(transfer) / 125000000.0 AS transfer_max
 FROM (
   SELECT log_time,
          SUM(object_size) AS transfer
--- bench2/q6/q6_druid.sql
+++ bench2/q6/q6_druid.sql
@@ -1,7 +1,7 @@
 -- Q2.6: What are the average and maximum data transfer rates (Gbps)?
 
-SELECT AVG(bandwidth) / 125000000.0 AS transfer_avg,
-       MAX(bandwidth) / 125000000.0 AS transfer_max
+SELECT AVG(transfer) / 125000000.0 AS transfer_avg,
+       MAX(transfer) / 125000000.0 AS transfer_max
 FROM (
   SELECT __time,
          SUM(object_size) AS transfer
